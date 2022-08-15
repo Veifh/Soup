7b57eb74c43801a8c40350b53ce8821b3b9e4aeb
Max <veifh4huger@gmail.com>
2022-08-15 23:17:31 +0400

    Update about task(123)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Progect
+{
+     class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 3;
+            Console.WriteLine(Convert.ToString(Math(a,b)));
+        }
+
+        public static int Math(int a, int b) 
+        {
+            return a + b;
+        }
+    }
+}
